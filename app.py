334557d79a856a8dc3408dcb6280277e58b4c175
Eljay5917 <elijahmawuliattakorah@gmail.com>
2023-08-31 15:37:55 +0000

    after the post request

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+
 
 
 app = Flask(__name__)
@@ -11,18 +12,24 @@ app = Flask(__name__)
 
 @app.route('/' )
 def index():
-    
+    weather =request.form.get("weather")
+    country =request.form.get("country")
+   
     return render_template("index.html")
 
 @app.route('/results')
+
 def results():
-     
+    weather =request.form.get("weather")
+    country =request.form.get("country")
+   
          
-    return render_template("Invalid request")
+    return render_template("results.html",weather=weather)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
